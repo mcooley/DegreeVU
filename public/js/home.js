79684f218798e90f77858aaf6aca76dd3c7eed48
Matt Cooley <matt@mattcooley.net>
2013-03-30 23:40:15 -0500

    started fleshing out Goal object

--- public/js/home.js
+++ public/js/home.js
@@ -11,12 +11,11 @@ $(document).ready(function () {
 		gradYear = 2016;
 	}
 	
-	var sched = new Schedule([], {gradYear:gradYear});
+	var sched = Schedule.getInstance(gradYear);
 
 	var scheduleView = new ScheduleView({collection: sched, el:'#scheduleGrid'});
 	scheduleView.render();
 	
-	/*
 	var majorId = getQueryString('major');
 	if (majorId) {
 		var	major = new Goal({
@@ -30,12 +29,12 @@ $(document).ready(function () {
 	}
 	
 
-	var goalsList = new GoalsList([major], {el:'#goals'});	
-	*/
+	//var goalsList = new GoalsList([major], {el:'#goals'});	
 	
-	var testCourse = new Course({id: '51571cbf9c27196b6293f335'});
+	
+	var testCourse = new Course({_id: '5157a473f99cc15bf8a06e92'});
 	testCourse.fetch();
-	var testCourse2 = new Course({id: '51571cd69c27196b6293f336'});
+	var testCourse2 = new Course({_id: '5157a473f99cc15bf8a06e93'});
 	testCourse2.fetch();
 	
 	window.setTimeout(function() {
@@ -44,6 +43,12 @@ $(document).ready(function () {
 		var view2 = new CourseView({model: testCourse2, el:'#testCourseHome2'});
 		view2.render();
 		
+		
+		var goalView = new GoalView({
+			model:major,
+			el:'#goal1'
+		});
+		goalView.render();
 		
 		$('.scheduleBlock').draggable({
 			appendTo: 'body',
@@ -74,10 +79,15 @@ $(document).ready(function () {
 });
 
 var Course = Backbone.Model.extend({
-	urlRoot: '/courses',
+	url: function() {
+		return '/courses/' + this.get('_id');
+	},
 	getColorId: function() {
 		//TODO: actually check the requirements and stuff
 		return 1;
+	},
+	getHours:function() {
+		return (this.get('numOfCredits'))[0];
 	}
 });
 
@@ -85,23 +95,26 @@ var CourseView = Backbone.View.extend({
 	pixelsPerHour: 21,
 	
 	initialize: function() {
-		this.model.on('change:semester', (this.onAddToSchedule).bind(this))
+		Schedule.getInstance().on('add remove reset', (this.onAddToSchedule).bind(this));
 	},
 	
 	render: function() {
 		this.$el.addClass('scheduleBlock');
 		this.$el.append('<div class="scheduleBlockHeader color' + this.model.getColorId() + '">' + this.model.get('courseNumber') + '</div><div class="scheduleBlockBody">' + this.model.get('courseName') + '</div>');
-		this.$el.find('.scheduleBlockBody').css('height', this.pixelsPerHour * ((this.model.get('numOfCredits'))[0] - 1));
+		this.$el.find('.scheduleBlockBody').css('height', this.pixelsPerHour * (this.model.getHours() - 1));
 		
 		this.$el.data('courseObj', this.model);
 	},
 	
 	onAddToSchedule: function() {
-		this.$el.draggable("disable").addClass('placed');
+		if (Schedule.getInstance().contains(this.model)) {
+			this.$el.draggable("disable").addClass('placed');
+		}
 	}
 });
 
 var Schedule = Backbone.Collection.extend({
+	model: Course,
 	initialize: function(models, options) {		
 		var gradYear = options.gradYear;
 		
@@ -120,17 +133,33 @@ var Schedule = Backbone.Collection.extend({
 	
 	getSemesters: function() {
 		return this._semesters;
+	},
+	
+	hasCourse: function(course) {
+		return this.contains(course);
+	}
+},
+{
+	_singletonInstance:null,
+	getInstance:function(gradYear) {
+		if (!this._singletonInstance) {
+			this._singletonInstance = new Schedule([], {gradYear:gradYear});
+		}
+		return this._singletonInstance;
 	}
 });
 
 var ScheduleView = Backbone.View.extend({
+	initialize: function() {
+		//this.collection.on('add remove reset', (this.updateHoursCount).bind(this));
+	},
 	
 	render: function() {
 		_.each(this.collection.getSemesters(), (function(semester) {
-			this.$el.append('<div class="scheduleCol ' + semester.season.toLowerCase() + '" data-semesterseason="' + semester.season + '" data-semesteryear="' + semester.year + '"><div class="scheduleColHeader">' + (semester.season || '') + (semester.year? ' ' : '') + (semester.year || '') + ' <span class="hoursCount">(0)</span></div><div class="scheduleColBody"></div></div>');
+			this.$el.append('<div class="scheduleCol ' + semester.season.toLowerCase() + '" data-semesterseason="' + semester.season + '" data-semesteryear="' + semester.year + '"><div class="scheduleColHeader">' + (semester.season || '') + (semester.year? ' ' : '') + (semester.year || '') + '</div><div class="scheduleColBody"></div></div>');
 		}).bind(this));
 		
-		this.$el.children('.scheduleCol').on('sortreceive', (this.onCourseMoved).bind(this)).on('sortbeforestop', (this.validateCourseMove).bind(this));
+		this.$el.children('.scheduleCol').on('sortreceive', (this.onCourseMoved).bind(this));
 	},
 	
 	onCourseMoved: function(event, ui) {
@@ -144,6 +173,8 @@ var ScheduleView = Backbone.View.extend({
 			this.collection.add(courseModel);
 		} else if (ui.sender.parent().is('.scheduleCol')) {
 			// trigger event on ui.item?
+			
+			// Problem: we don't have a reference to the courseModel.
 			
 			// Moving course within the schedule
 			console.log('Move course within schedule.');
@@ -165,19 +196,43 @@ var ScheduleView = Backbone.View.extend({
 
 // Goals stuff (not yet implemented)
 var GoalList = Backbone.Collection.extend({
-	
+	model:Goal
 });
 
 var GoalListView = Backbone.View.extend({
-	render: function() {
-		
-	}
+
+});
+
+
+var CourseCollection = Backbone.Collection.extend({
+	model:Course
 });
 
 var Goal = Backbone.Model.extend({
-	urlRoot: '/goals'
+	urlRoot: '/goals',
+	
+	initialize:function() {
+		this.on('sync', (this.loadCourses).bind(this));
+	},
+	
+	loadCourses:function() {
+		console.log('ran loadCourses!');
+		_.each(this.get('items'), (function(item, i, items) {
+			console.log(item);
+			var courseCollection = new CourseCollection([], {
+				url: '/courses/lookup?q=' + item.courses.map(encodeURIComponent).join(',')
+			});
+			courseCollection.fetch();
+			
+		}).bind(this));
+	}
 });
 
 var GoalView = Backbone.View.extend({
-	
+	render: function() {
+		_.each(this.model.get('items'), (function(item, i) {
+			this.$el.append('<div class="goalSection color' + ((i % 9) + 1) + '"><h2>' + item.title + '</h2></div>');
+			//TODO: deal with item.courses
+		}).bind(this));
+	}
 });
