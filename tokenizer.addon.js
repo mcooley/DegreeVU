107826d702a6ed7a2f6bb331ec2ef9befc68c95e
Brendan McNamara <brendan@streampiece.com>
2013-09-15 12:32:58 -0500

    Defines methods for StatementHelper

--- tokenizer.addon.js
+++ tokenizer.addon.js
@@ -1,127 +1,40 @@
-var extend = function(obj) {
-	
-}
-
 //add ons to tokenizer for server-side querying
 //include require statement here
+var StatementHelper = {};
+StatementHelper.addTokenToQuery = function(token, mongoQuery) {
 
+};
+
+//the number of copies held in the query
+StatementHelper.queryLength = function(mongoQuery) {
+
+};
+
+//makes a deep copy of the query
+StatementHelper.copyQuery = function(mongoQuery) {
+
+};
+
+//takes a query for a single course code and converts it 
+//to regexp
+StatementHelper.queryToRegExp = function(query) {
+	if (query.isSingleCourse()) {
+		//do something, otherwise don't
+	} 
+};
+
+//diagnoses the query and determines the number of copies
+//at the root it will need based on the complexity of the query
+StatementHelper.copiesNeeded = function(statementArr) {
+
+};
 //generates a mongo query from this query
 
-Query.extend = function(obj) {
-	var i, deepCopy = {};
-	if (i in obj) {
-		if (obj.hasOwnProperty(i)) {
-			if (typeof obj[i] === 'object') {
-				deepCopy[i] = Query.extend(obj[i]);
-			} else {
-				deepCopy[i] = obj[i];
-			}
-		}
-	}
-	return deepCopy;
-}
+
 Query.prototype.mongoQuery = function() {
-	var mongoQuery = {};
-	mongoQuery.$or = [];
-
-
-	this.array.forEach(function(token) {
-		var courseCode;
-		if (!token.query) {
-			courseCode = "^"+token.coursePrefix+"\\s*"+token.courseNumber.toString();
-			courseCode += (token.courseSuffix) ? ("\\s*" + token.courseSuffix) : "";
-
-			if (token.not) {
-				if (!mongoQuery.courseCode) {
-					mongoQuery.courseCode = {};
-				}
-
-				if (!mongoQuery.courseCode.$nin) {
-					mongoQuery.courseCode.$nin = [];
-				}
-				mongoQuery.courseCode.$nin.push(new RegExp(courseCode, 'i'));
-
-			} else {
-				if (!mongoQuery.courseCode) {
-					mongoQuery.courseCode = {};
-				}
-
-				if (!mongoQuery.courseCode.$in) {
-					mongoQuery.courseCode.$in = [];
-				}
-				//should only have 1 element in it, or there is a problem with the query
-				mongoQuery.courseCode.$in.push(new RegExp(courseCode, 'i'));
-			}
-				
-		} else if (token.query === '+') {
-			//above
-			if (token.not) {
-				//then it gets complicated...
-			} else {
-
-			}
-
-		} else if (token.query === '*') {
-			//all
-			if (!mongoQuery.coursePrefix) {
-				mongoQuery.coursePrefix = {};
-			}
-			if (token.not) {
-
-				if (!mongoQuery.coursePrefix.$nin) {
-					mongoQuery.coursePrefix.$nin = []''
-				}
-				mongoQuery.coursePrefix.$nin.push(new RegExp(token.coursePrefix, 'i'));
-			} else {
-				if (!mongoQuery.coursePrefix.$in) {
-					mongoQuery.coursePrefix.$in = [];
-				}
-				mongoQuery.coursePrefix.$in.push(new RegExp(token.coursePrefix, 'i'));
-			}
-
-		} else if (token.query === '$') {
-			//suffix
-			if (token.not) {
-				if (!mongoQuery.suffix) {
-					mongoQuery.suffix = {};
-				}
-
-				if (!mongoQuery.suffix.$nin) {
-					mongoQuery.suffix.$nin = [];
-				}
-
-				mongoQuery.suffix.$nin.push(new RegExp(token.suffix, 'i'));
-			} else {
-				if (!mongoQuery.suffix) {
-					mongoQuery.suffix = {};
-				}
-
-				if (!mongoQuery.suffix.$in) {
-					mongoQuery.suffix.$in = [];
-				}
-				mongoQuery.suffix.$in.push(new RegExp(token.suffix, 'i'));
-			}
-
-		} else if (token.query === '~') {
-			//category
-			if (!mongoQuery.category) {
-				mongoQuery.category = {};
-			}
-			if (token.not) {
-				if (!mongoQuery.category.$nin) {
-					mongoQuery.category.$nin = [];
-				}
-				mongoQuery.category.$nin.push(token.category);
-			} else {
-				if (!mongoQuery.category.$in) {
-					mongoQuery.category.$in = [];
-				}
-				mongoQuery.category.$in.push(token.category);
-			}
-		}
-	});
-}
+	
+};
 
 QueryCollection.prototype.mongoQuery = function() {
 
-}
+};
