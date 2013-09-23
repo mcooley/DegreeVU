ac1e62636cad2cfd44b34af4e62c2d0a83da37a4
Brendan McNamara <brendan@streampiece.com>
2013-09-22 22:39:51 -0500

    Implements copyMongoQuery method of statement helper

--- tokenizer.addon.js
+++ tokenizer.addon.js
@@ -1,18 +1,32 @@
 //add ons to tokenizer for server-side querying
 //include require statement here
 var StatementHelper = {};
-StatementHelper.addTokenToQuery = function(token, mongoQuery) {
+StatementHelper.addTokenToMongoQuery = function(token, mongoQuery) {
 
 };
 
 //the number of copies held in the query
-StatementHelper.queryLength = function(mongoQuery) {
+StatementHelper.mongoQueryLength = function(mongoQuery) {
 
 };
 
 //makes a deep copy of the query
-StatementHelper.copyQuery = function(mongoQuery) {
-
+StatementHelper.copyMongoQuery = function(mongoQuery) {
+	var i, copy = {};
+	if (typeof mongoQuery === 'object') {
+		for (i in mongoQuery) {
+			if (mongoQuery.hasOwnProperty(i)) {
+				if (Array.isArray(mongoQuery[i])) {
+					copy[i] = mongoQuery[i].slice();
+				} else {
+					copy[i] = StatementHelper.copyMongoQuery(mongoQuery[i]);
+				}
+					
+			}
+		}
+		return copy;
+	}
+	return mongoQuery;
 };
 
 //takes a query for a single course code and converts it 
