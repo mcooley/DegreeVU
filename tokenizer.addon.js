f151c82189f9c69da1cdab16ebabdc6a28f182ed
Brendan McNamara <brendan@streampiece.com>
2013-09-30 13:50:35 -0500

    Reorders methods of statement helper

--- tokenizer.addon.js
+++ tokenizer.addon.js
@@ -7,13 +7,13 @@ var Tokenizer = require('./public/js/tokenizer'),
 //include require statement here
 
 //returns the mongoQuery after the token has been added
-var StatementHelper = {};
-StatementHelper.addTokenToMongoQuery = function(token, mongoQuery) {
+var StatementHelper = {
+	addTokenToMongoQuery = function(token, mongoQuery) {
 	
 		if (!token.query) {
 			mongoQuery.courseCode = (token.not) ? {$nin: [tokenToRegExp(token)]} : StatementHelper.tokenToRegExp(token);
 		} else if (token.query === '*') {
-			mongoQuery.coursePrefix = (token.not) ? {$nin: [new RegExp(token.coursePrefix, "i")]} : new RegExp(token.coursePrefix);
+			mongoQuery.coursePrefix = (token.not) ? {$nin: [new RegExp("^"+token.coursePrefix+"$", "i")]} : new RegExp("^"+token.coursePrefix+"$", "i");
 		} else if (token.query === '+') {
 			if (mongoQuery.courseCode) {
 				if (token.not && mongoQuery.courseCode.$nin) {
@@ -37,185 +37,180 @@ StatementHelper.addTokenToMongoQuery = function(token, mongoQuery) {
 				mongoQuery.courseCode = (token.not) ? {$nin: [StatementHelper.plusTokenToRegExp(token)]} : {$in: [StatementHelper.plusTokenToRegExp(token)]};
 			}
 		} else if (token.query === '$') {
-			mongoQuery.courseSuffix = new RegExp(token.courseSuffix, "i");
+			mongoQuery.courseSuffix = new RegExp("^"+token.courseSuffix+"$", "i");
 		} else if (token.query === '^') {
 			//school name
 		} else if (token.query === '~') {
 			//category
 			if (mongoQuery.category) {
 				if (token.not && mongoQuery.category.$nin) {
-					mongoQuery.category.$nin.push(new RegExp(token.category, "i"));
+					mongoQuery.category.$nin.push(new RegExp("^"+token.category+"$", "i"));
 				} else if (token.not && !mongoQuery.category.$nin) {
-					mongoQuery.category.$nin = [new RegExp(token.category, "i")];
+					mongoQuery.category.$nin = [new RegExp("^"+token.category+"$", "i")];
 				} 
 				//for !token.not
 				else if (mongoQuery.category.$in) {
-					mongoQuery.category.$in.push(new RegExp(token.category, "i"));
+					mongoQuery.category.$in.push(new RegExp("^"+token.category+"$", "i"));
 				} 
 				// !mongoQuery.category.$in
 				else {
-					mongoQuery.category.$in = [new RegExp(token.category, "i")];
+					mongoQuery.category.$in = [new RegExp("^"+token.category+"$", "i")];
 				}
 			} else {
-				mongoQuery.category = (token.not) ? {$nin: [new RegExp(token.category, "i")]} : {$in: [new RegExp(token.category, "i")]};
+				mongoQuery.category = (token.not) ? {$nin: [new RegExp("^"+token.category+"$", "i")]} : {$in: [new RegExp("^"+token.category+"$", "i")]};
 			}
 		}
 		
-};
-
-//makes a deep copy of the query
-StatementHelper.copyMongoQuery = function(mongoQuery) {
-	var i, copy = {};
-	if (typeof mongoQuery === 'object') {
-		for (i in mongoQuery) {
-			if (mongoQuery.hasOwnProperty(i)) {
-				if (Array.isArray(mongoQuery[i])) {
-					copy[i] = mongoQuery[i].slice();
-				} else {
-					copy[i] = StatementHelper.copyMongoQuery(mongoQuery[i]);
+	},
+	//makes a deep copy of the query
+	copyMongoQuery = function(mongoQuery) {
+		var i, copy = {};
+		if (typeof mongoQuery === 'object') {
+			for (i in mongoQuery) {
+				if (mongoQuery.hasOwnProperty(i)) {
+					if (Array.isArray(mongoQuery[i])) {
+						copy[i] = mongoQuery[i].slice();
+					} else {
+						copy[i] = StatementHelper.copyMongoQuery(mongoQuery[i]);
+					}
+						
 				}
-					
 			}
+			return copy;
 		}
-		return copy;
-	}
-	return mongoQuery;
-};
+		return mongoQuery;
+	},
+	//takes a query for a single course code and converts it 
+	//to regexp, this method only converts single courses to regexp
+	//ignores the not property on the token
+	tokenToRegExp = function(token) {
+		var courseSearch;
+		if (!token.query) {
+			courseSearch = "^" + token.coursePrefix + '(\\s?)+' + token.courseNumber.toString();
+			courseSearch += (token.courseSuffix) ? token.courseSuffix  + '$' : '$';
+		} else if (token.query === '*') {
+			courseSearch = "^" + token.coursePrefix + "\\d+";
+		} else if (token.query === '+') {
+			courseSearch = "^" + token.coursePrefix + "";
 
-//takes a query for a single course code and converts it 
-//to regexp, this method only converts single courses to regexp
-//ignores the not property on the token
-StatementHelper.tokenToRegExp = function(token) {
-	var courseSearch;
-	if (!token.query) {
-		courseSearch = "^" + token.coursePrefix + '(\\s?)+' + token.courseNumber.toString();
-		courseSearch += (token.courseSuffix) ? token.courseSuffix  + '$' : '$';
-	} else if (token.query === '*') {
-		courseSearch = "^" + token.coursePrefix + "\\d+";
-	} else if (token.query === '+') {
-		courseSearch = "^" + token.coursePrefix + "";
-
-	}
-		
-	return new RegExp(courseSearch, "i");
-};
-
-//takes a plus query and converts it to regexp
-//this is separated from the tokenToRegExp method because
-//it is much more complex
-StatementHelper.plusTokenToRegExp = function(token) {
-	var courseSearch,
-		numberSearch,
-		digits;
-	if (token.query !== '+') {
-		throw new Error("Expecting a plus token to be passed in method plusQueryToRegExp");
-	}
-	courseSearch = "^" + token.coursePrefix + '\\s';
-	//all number regexes should work for numbers with 2, 3, or 4 digits
-	courseSearch += "(";
-	courseSearch += ("(" + StatementHelper.numberSearchGenerator(token.courseNumber, 4) + ")");
-	if (token.courseNumber < 1000) {
-		courseSearch += ("|(" + StatementHelper.numberSearchGenerator(token.courseNumber, 3) + ")");
-	}
-	if (token.courseNumber < 100) {
-		courseSearch += ("|(" + StatementHelper.numberSearchGenerator(token.courseNumber, 2) + ")");
-	}	
-	courseSearch += ")";
-
-	//could have a course suffix at the end of the code
-	courseSearch += "[a-z]*";
-	return new RegExp(courseSearch, 'i');
-
-};
-
-//generates a string that can be converted to regexp for searching
-//for a number greater than or equal to the parameter, with the number of digits used
-//to search
-StatementHelper.numberSearchGenerator = function(num, numOfDigits) {
-	var number = num,
-		digits = [],
-		numberSearch, i, j, n, digitsCount,
-		searchCombinations, next;
-	//store all the digits in an array
-	while (number != 0) {
-		digits.push(number % 10);
-		number = Math.floor(number / 10);
-	}
-	
-	//special case where the number 0 is passed in
-	if (digits.length === 0) {
-		numberSearch = "";
-		for (i = 0, n = numOfDigits; i < n; ++i) {
-			numberSearch += "\\d";
 		}
+			
+		return new RegExp(courseSearch, "i");
+	},
+	plusTokenToRegExp = function(token) {
+		var courseSearch,
+			numberSearch,
+			digits;
+		if (token.query !== '+') {
+			throw new Error("Expecting a plus token to be passed in method plusQueryToRegExp");
+		}
+		courseSearch = "^" + token.coursePrefix + '\\s';
+		//all number regexes should work for numbers with 2, 3, or 4 digits
+		courseSearch += "(";
+		courseSearch += ("(" + StatementHelper.numberSearchGenerator(token.courseNumber, 4) + ")");
+		if (token.courseNumber < 1000) {
+			courseSearch += ("|(" + StatementHelper.numberSearchGenerator(token.courseNumber, 3) + ")");
+		}
+		if (token.courseNumber < 100) {
+			courseSearch += ("|(" + StatementHelper.numberSearchGenerator(token.courseNumber, 2) + ")");
+		}	
+		courseSearch += ")";
 
-		return numberSearch;
-	} 
-	//make sure that the number of digits being passed in,
-	//can generate a number that is greater than or equal to 
-	//the number
-	else if (digits.length <= numOfDigits) {
-		searchCombinations = [];
+		//could have a course suffix at the end of the code
+		courseSearch += "[a-z]*";
+		return new RegExp(courseSearch, 'i');
+
+	},
+	//generates a string that can be converted to regexp for searching
+	//for a number greater than or equal to the parameter, with the number of digits used
+	//to search
+	numberSearchGenerator = function(num, numOfDigits) {
+		var number = num,
+			digits = [],
+			numberSearch, i, j, n, digitsCount,
+			searchCombinations, next;
+		//store all the digits in an array
+		while (number != 0) {
+			digits.push(number % 10);
+			number = Math.floor(number / 10);
+		}
 		
-		for (i = 0, n = numOfDigits; i < n; ++i) {
+		//special case where the number 0 is passed in
+		if (digits.length === 0) {
+			numberSearch = "";
+			for (i = 0, n = numOfDigits; i < n; ++i) {
+				numberSearch += "\\d";
+			}
+
+			return numberSearch;
+		} 
+		//make sure that the number of digits being passed in,
+		//can generate a number that is greater than or equal to 
+		//the number
+		else if (digits.length <= numOfDigits) {
+			searchCombinations = [];
 			
-			next = "(";
-			
-			for (j = numOfDigits - 1; j >= 0; --j) {
-				if (j >= digits.length) {
-					if (j > i) {
-						next += "0";
-					} else  {
-						next += "[^0]";
-					} 
-				} else {
-					if (j > i) {
-						next += digits[j].toString();
-					} else if (j === i) {
-						next += (j === 0) ? StatementHelper.numberGenerator(digits[j], true) : StatementHelper.numberGenerator(digits[j]);
-					} else { //j is less than i
-						next += "\\d";
+			for (i = 0, n = numOfDigits; i < n; ++i) {
+				
+				next = "(";
+				
+				for (j = numOfDigits - 1; j >= 0; --j) {
+					if (j >= digits.length) {
+						if (j > i) {
+							next += "0";
+						} else  {
+							next += "[^0]";
+						} 
+					} else {
+						if (j > i) {
+							next += digits[j].toString();
+						} else if (j === i) {
+							next += (j === 0) ? StatementHelper.numberGenerator(digits[j], true) : StatementHelper.numberGenerator(digits[j]);
+						} else { //j is less than i
+							next += "\\d";
+						}
 					}
 				}
+				next += ")";
+				searchCombinations.push(next);
 			}
-			next += ")";
-			searchCombinations.push(next);
+			//remove any combinations where there is a 9 at the digit
+			searchCombinations = searchCombinations.filter(function(comb, index) {
+				return (index === 0 || index >= digits.length || digits[index] !== 9);
+			});
+			return "(" + searchCombinations.join("|") + ")";
 		}
-		//remove any combinations where there is a 9 at the digit
-		searchCombinations = searchCombinations.filter(function(comb, index) {
-			return (index === 0 || index >= digits.length || digits[index] !== 9);
-		});
-		return "(" + searchCombinations.join("|") + ")";
+		return null;
+	},
+	//pass in a single digit number and this will generate
+	//a string of numbers that are greater than or equal to the
+	//string digit number, with no breaks between the numbers
+	//used to help generate plusQuery regexp's, should work if passing in
+	//a number or a string
+	//include number is a boolean that determines if the number itself is included
+	//in the string or if it is a string of numbers > than the number.  This will
+	//not have the correct behavior if the num passed in is greater than 9
+	numberGenerator = function(digit, includeNumber) {
+		var number = (typeof num === 'number') ? digit : +digit;
+		if (!includeNumber) {
+			number++;
+		}
+		if (number === 0) {
+			return "\\d"
+		} 
+		if (number === 9) {
+			return "9";
+		}
+		
+		return "["+number.toString()+"-9]";
+		
+		
 	}
-	return null;
 };
-//pass in a single digit number and this will generate
-//a string of numbers that are greater than or equal to the
-//string digit number, with no breaks between the numbers
-//used to help generate plusQuery regexp's, should work if passing in
-//a number or a string
-//include number is a boolean that determines if the number itself is included
-//in the string or if it is a string of numbers > than the number.  This will
-//not have the correct behavior if the num passed in is greater than 9
-StatementHelper.numberGenerator = function(digit, includeNumber) {
-	var number = (typeof num === 'number') ? digit : +digit;
-	if (!includeNumber) {
-		number++;
-	}
-	if (number === 0) {
-		return "\\d"
-	} 
-	if (number === 9) {
-		return "9";
-	}
-	
-	return "["+number.toString()+"-9]";
+
 	
 	
-}
 //generates a mongo query from this query
-
-
 Statement.prototype.mongoQuery = function() {
 	var query = {};
 	this.tokens.forEach(function(token) {
