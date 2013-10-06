eb50ccce72fe676f7dacb1fc2ee6e38744942a9c
Brendan McNamara <brendan@streampiece.com>
2013-10-06 16:23:21 -0500

    Removes outdated tests for models and goals and cleans up empty specs

--- spec-client/schedule.spec.js
+++ spec-client/schedule.spec.js
@@ -1,3 +1,8 @@
 describe("Schedule Testing Suite:", function() {
-
+	it("should succeed", function() {
+		expect(true).toBeTruthy();
+	});
+	it('should fail', function() {
+		expect(true).toBeFalsy();
+	});
 });
