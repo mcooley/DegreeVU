949428b287c0d8c1b15a7df50d3a03ab389ce0cb
Brendan McNamara <brendan@streampiece.com>
2013-10-10 12:27:46 -0500

    Adds some documentation to course and course collection

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,18 +1,24 @@
+
+/**
+ * Singleton Object containing all the data pertaining to a single course
+ * @class Course
+ */
 var Course = Backbone.UniqueModel(Backbone.Model.extend({
 	idAttribute: '_id',
-	initialize: function() {
-		
-	},
 	url: function() {
 		return '/courses/' + this.get('_id');
 	},
-	
 	getHours:function() {
 		return (this.get('numOfCredits'))[0];
-	}
+	},
+
 	
 }));
 
+/**
+ * Collection of Course Objects
+ * @class
+ */
 var CourseCollection = Backbone.Collection.extend({
 	model:Course,
 	initialize: function(models, options) {
@@ -25,6 +31,15 @@ var CourseCollection = Backbone.Collection.extend({
 		}).bind(this));
 	},
 
+	/**
+	 * Fetches courses from the server and places them caches them into the
+	 * Course Collection.
+	 * @method fetchCourses
+	 * @event sync
+	 * @async
+	 * @param {Array, StatementCollection} An array or statement collection containing the
+	 * courses that are being fetched from the server
+	 */
 	fetchCourses: function(courses) {
 		var statementCollection;
 		if (Array.isArray(courses)) {
