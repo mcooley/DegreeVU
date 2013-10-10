d18eeb1184531dd555b6ced3935fe73d9e397fd2
Brendan McNamara <brendan@streampiece.com>
2013-10-10 12:37:08 -0500

    Adds lock methods to course object

--- public/js/courses.js
+++ public/js/courses.js
@@ -12,12 +12,50 @@ var Course = Backbone.UniqueModel(Backbone.Model.extend({
 		return (this.get('numOfCredits'))[0];
 	},
 
+	/**
+	 * Adds lock to the Goal with the id passed in
+	 * this method does nothing if the course is already 
+	 * locked for this id
+	 * @method addLock
+	 * @param {String} id the id of the Goal that is being locked
+	 */
+	addLock:function(id) {
+		if (!this.locks) {
+			this.locks = [id];
+		} else if (!this.isLocked()) {
+			this.locks.push(id);
+		}
+	},
+
+	/**
+	 * Reset all the locks that exist on the Course
+	 * @method reset
+	 */
+	reset: function() {
+		this.locks = null;
+	},
+	/**
+	 * Checks if the course is locked to the Goal
+	 * with the given id
+	 * @method isLocked
+	 * @param {String} id the id of the Goal
+	 * @return {Boolean} true if the Course is locked, false otherwise
+	 */
+	isLocked: function(id) {
+		if (!this.locks) {
+			return false;
+		} else {
+			return this.locks.reduce(function(memo, lockedID) {
+				return memo || lockedID === id;
+			}, false);
+		}
+	},
 	
 }));
 
 /**
  * Collection of Course Objects
- * @class
+ * @class CourseCollection
  */
 var CourseCollection = Backbone.Collection.extend({
 	model:Course,
