4785356fad2fd138439656d68e029de0ddaf7aeb
Matt Cooley <matt@mattcooley.net>
2013-10-10 20:27:22 -0500

    Removed JQuery UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,12 @@
-
-//define dependencies and paths
 require.config({
 	baseURL: './js',
 	paths: {
 		jquery: '../lib/jquery/jquery-1.9.1',
-		jquery_ui: '../lib/jquery-ui/jquery-ui-1.10.2.custom',
 		underscore: '../lib/underscore/underscore-min',
 		backbone: '../lib/backbone/backbone-min',
 		backbone_uniquemodel: '../lib/backbone-uniqueModel/backbone.uniquemodel',
 		tpl: '../lib/requirejs-tpl/tpl'
 	},
-	//list dependencies
 	shim: {
 		'backbone': {
             deps: ['underscore', 'jquery'],
@@ -23,11 +19,6 @@ require.config({
 			deps: ['backbone'],
 			exports: 'Backbone.UniqueModel'
 		}
-		//'jquery_ui': ['jquery'],
-		//'backbone': ['underscore', 'jquery']
-		//'backbone_uniquemodel': ['backbone'],
-		//'courses': ['backbone_uniquemodel','backbone'],
-		//'views': ['goals', 'backbone', 'jquery_ui']
 	}
 });
 
